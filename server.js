9113cd3d39fd5c37f690212b17370a6f101090b2
Kingsten Banh <kingstenbanh@gmail.com>
2015-02-20 12:41:40 -0700

    add express server

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+var port = process.env.PORT || 8000;
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'app')));
+
+app.listen(port, function() {
+  console.log('Express server listening on port ', port);
+});
